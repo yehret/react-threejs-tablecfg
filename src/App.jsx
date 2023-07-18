fb24414c327e1ac3714a8cfebd24304f691280f9
Serhii <serhii.samoliuk@outlook.com>
2023-07-18 22:17:03 +0200

    add stage properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Canvas } from '@react-three/fiber';
+import { Experience } from './components/Experience';
+import { SoftShadows } from '@react-three/drei';
 
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas shadows camera={{ position: [4, 0, -12], fov: 35 }}>
+        {/* <SoftShadows samples={20} /> */}
         <Experience />
       </Canvas>
     </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stage } from '@react-three/drei';
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      <Stage
+        intensity={1.5}
+        environment="sunset"
+        shadows={{ type: 'accumulative', color: '#d9afd9', colorBlend: 2, opacity: 2 }}
+        adjustCamera={2}>
+        <mesh castShadow>
+          <meshNormalMaterial />
+          <boxGeometry />
+        </mesh>
+      </Stage>
+      <OrbitControls makeDefault mixPolarAngle={0} maxPolarAngle={Math.PI / 2} />
     </>
   );
 };
